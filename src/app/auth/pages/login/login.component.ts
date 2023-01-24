6a58f943350653fae1400b5c279dfe9c413ef5f4
Enrique Padilla <ultrared808@gmail.com>
2023-01-24 10:54:39 -0600

    bug_01

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { AuthService } from '../../services/auth.service';
-//import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,18 +11,23 @@ export class LoginComponent implements OnInit {
 
   form!: FormGroup;
 
-  constructor(private asAuthService: AuthService) {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
     this.form = new FormGroup({
-      email: new FormControl('',[Validators.required, Validators.email]),
-      password: new FormControl('',[Validators.required, Validators.minLength(6), Validators.maxLength(12)])
+      email: new FormControl('someaccount@domain.com',[Validators.required, Validators.email]),
+      password: new FormControl('admin123',[Validators.required, Validators.minLength(6), Validators.maxLength(12)])
     })
   }
 
   ingresar() {
-    const { email,password } = this.form.value;
-    this.asAuthService.sendCredentials(email,password);
+    const {email, password} = this.form.value;
+
+    if (email=='someaccount@domain.com'&& password=='admin123') {
+      this.router.navigate(['/cursos']);
+    } else {
+      this.router.navigate(['/404']);
+    }
   }
 
 }
